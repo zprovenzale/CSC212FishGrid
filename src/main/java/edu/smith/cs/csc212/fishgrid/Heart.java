6e07ae3e4db4813d1b7b8dd58146c53e9c746c19
John Foley <jjfoley@smith.edu>
2020-02-14 12:08:40 -0500

    Get rid of comments on Heart.java

--- src/main/java/edu/smith/cs/csc212/fishgrid/Heart.java
+++ src/main/java/edu/smith/cs/csc212/fishgrid/Heart.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 /**
- * This class is provided so that you don't have to make any drawing code yourself!
+ * This class is provided so that you don't have to make any drawing code
+ * yourself!
+ * 
  * @author jfoley
  *
  */
@@ -18,10 +20,8 @@ public class Heart extends WorldObject {
 	public void draw(Graphics2D g) {
 		int size = 36;
 		Graphics2D scale = (Graphics2D) g.create();
-		scale.scale(1.0/(double) size, 1.0/(double) size);
-		scale.translate(-size/2, -size/2);
-		
-		// Feel free to draw something else if you want.
+		scale.scale(1.0 / (double) size, 1.0 / (double) size);
+		scale.translate(-size / 2, -size / 2);
 		scale.setColor(Color.red);
 		drawHeart(scale, 10, 10, 16, 16);
 	}
@@ -30,35 +30,23 @@ public class Heart extends WorldObject {
 	public void step() {
 
 	}
-	
+
 	/**
 	 * Draw a heart. Sourced from: https://stackoverflow.com/a/38191657/1057048
-	 * @param g - the graphics object.
-	 * @param x - the position of the heart (center)
-	 * @param y - the position of the heart (center)
+	 * 
+	 * @param g      - the graphics object.
+	 * @param x      - the position of the heart (center)
+	 * @param y      - the position of the heart (center)
 	 * @param width  - the width of it
 	 * @param height - the height of it
 	 */
 	private static void drawHeart(Graphics2D g, int x, int y, int width, int height) {
-	    int[] triangleX = {
-	            x - 2*width/18,
-	            x + width + 2*width/18,
-	            (x - 2*width/18 + x + width + 2*width/18)/2};
-	    int[] triangleY = { 
-	            y + height - 2*height/3, 
-	            y + height - 2*height/3, 
-	            y + height };
-	    g.fillOval(
-	            x - width/12,
-	            y, 
-	            width/2 + width/6, 
-	            height/2); 
-	    g.fillOval(
-	            x + width/2 - width/12,
-	            y,
-	            width/2 + width/6,
-	            height/2);
-	    g.fillPolygon(triangleX, triangleY, triangleX.length);
+		int[] triangleX = { x - 2 * width / 18, x + width + 2 * width / 18,
+				(x - 2 * width / 18 + x + width + 2 * width / 18) / 2 };
+		int[] triangleY = { y + height - 2 * height / 3, y + height - 2 * height / 3, y + height };
+		g.fillOval(x - width / 12, y, width / 2 + width / 6, height / 2);
+		g.fillOval(x + width / 2 - width / 12, y, width / 2 + width / 6, height / 2);
+		g.fillPolygon(triangleX, triangleY, triangleX.length);
 	}
 
 }
